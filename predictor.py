d458d299ec1876d42e9b47510dd22055087f358f
lakshmana deepesh <pldeepesh@gmail.com>
2017-12-27 23:03:56 +0530

    added code,config files for DB and included gitignore

--- predictor.py
+++ predictor.py
@@ -2,4 +2,14 @@ import json
 import requests as re
 from datetime import datetime
 
-url
+
+if __name__ == '__main__':
+	url = 'https://blockchain.info/tobtc'
+	headers = {'User-agent':'Mozilla 5.0'}
+	creds = json.load(open('config.json'))
+
+	response = re.get(url,headers=headers)
+	data = json.loads(response.text)
+	print(data)
+
+
