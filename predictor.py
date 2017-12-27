fa5b07570a4c936c7a565250fa04e7fa90451179
lakshmana deepesh <pldeepesh@gmail.com>
2017-12-27 23:30:09 +0530

    finished the changes for DB and completed the code

--- predictor.py
+++ predictor.py
@@ -1,15 +1,21 @@
 import json
 import requests as re
 from datetime import datetime
+import psycopg2 as con
 
 
 if __name__ == '__main__':
-	url = 'https://blockchain.info/tobtc'
+	url = 'https://blockchain.info/ticker'
 	headers = {'User-agent':'Mozilla 5.0'}
 	creds = json.load(open('config.json'))
 
-	response = re.get(url,headers=headers)
-	data = json.loads(response.text)
-	print(data)
+	resp = re.get(url,headers=headers)
+	data = json.loads(resp.text)
+	conn = con.connect(user=creds['redshift']['user'],host=creds['redshift']['host'],port=creds['redshift']['port'],password=creds['redshift']
+		['password'],database=creds['redshift']['database'])
+	cursor = conn.cursor()
+	cursor.execute("insert into bitcoin_15(date,price,buy,sell) values(now(),%s,%s,%s);",(data['INR']['last'],data['INR']['buy'],data['INR']['sell']) )
+	conn.commit()
+	conn.close()
 
 
